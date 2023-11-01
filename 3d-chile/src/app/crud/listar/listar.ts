e9638d1fce195095111a626e6d9e63d02651b64f
piero <pie.valdes@duocuc.cl>
2023-11-01 06:06:54 -0300

    Modificar funciona, cambios visuales

--- 3d-chile/src/app/crud/listar/listar.ts
+++ 3d-chile/src/app/crud/listar/listar.ts
@@ -2,16 +2,27 @@ import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { Producto } from 'src/app/modelo/producto';
 import { FirestoreService } from 'src/app/services/firestore.service';
-
+import { AlertController } from '@ionic/angular';
 @Component({
     selector: 'app-listar',
     templateUrl: './listar.html',
     styleUrls:['./listar.scss']
 })
 export class ListarPage implements OnInit{
-    constructor(public navCtrl:NavController, private database : FirestoreService){
+    constructor(private altCtrl:AlertController,public navCtrl:NavController, private database : FirestoreService){
 
     }
+
+
+    alertConfirmacion(accion:string){
+      return {
+        header:'Exitoso',
+        message:`Producto ${accion} exitosamente!`,
+        buttons:['Ok']
+      }
+    }
+    
+
     productos:Producto[]=[];
     ngOnInit() {
         this.getProductos();
@@ -27,7 +38,56 @@ export class ListarPage implements OnInit{
         })
       }
 
-      eliminarProducto(prod:Producto){
-        this.database.eliminarProd(prod.id);
+     async eliminarProducto(prod:Producto){
+        const alert=await this.altCtrl.create({
+          header:'Precaucion',
+          message:'¿Seguro que quieres eliminar este producto?'
+          ,buttons:[
+            {
+              text:'Eliminar',
+              handler:async ()=>{
+                this.database.eliminarProd(prod.id);
+                const alert2=await this.altCtrl.create(this.alertConfirmacion('eliminado'));
+                alert2.present();
+              }
+            },
+            {
+              text:'Cancelar'
+            }
+          ]
+        });
+        alert.present();
+       
       }
+
+    async modificarProducto(prod:Producto){
+      const alert=await this.altCtrl.create({
+        header:`Modificar producto(${prod.nombre})`,
+        inputs:[
+          {type:'text', label:'nombre',value:prod.nombre, name:'nombre'},
+          {type:'text', label:'descripcion',value:prod.descripcion, name:'desc'},
+          {type:'text', label:'precio',value:prod.precio,name:'precio'},
+        ],
+        buttons:[
+          {
+            text:'Modificar',
+            handler:(res)=>{
+              let datos={
+                nombre:res.nombre,
+                descripcion:res.desc,
+                precio:parseInt(res.precio),
+                id:prod.id
+              }
+              this.database.modificarProd(prod.id,datos);
+            }
+          },
+          {
+            text:'Cancelar'
+          }
+        ]
+      });
+      
+      alert.present();
+      //this.database.modificarProd;
+    }
 }
--- 3d-chile/src/app/home/home.page.ts
+++ 3d-chile/src/app/home/home.page.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit } from '@angular/core';
+import { FirestoreService } from '../services/firestore.service';
+import { Producto } from '../modelo/producto';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  constructor() {}
 
+  productos:Producto[]=[];
+
+  constructor(private database:FirestoreService) {}
+  
+  ngOnInit(){
+    this.getProductos();
+  }
+  
+  getProductos(){
+this.database.obtenerColeccion<Producto>('Productos').subscribe((res)=>{
+  this.productos=res;
+})
+  }
 }
